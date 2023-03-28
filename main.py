1754168cd834ed2994006fcea9688b5d96e7a971
Dogukan Mertoglu <74355983+dokick@users.noreply.github.com>
2023-03-28 08:51:58 +0200

    Added 0 to 9

--- main.py
+++ main.py
@@ -19,10 +19,17 @@ def sum_of_set(iterable: Iterable) -> int:
 
 
 def main() -> None:
+    # Sum of set with all numbers that contain 1...5 without repetition
     print(sum_of_set(range(1, 6)))  # 3.999.960
     print(66_666 * factorial(5) / 2)  # 3.999.960
+
+    # Sum of set with all numbers that contain 1...8 without repetition
     print(sum_of_set(range(1, 9)))  # 2.015.999.979.840
     print(99_999_999 * factorial(8) / 2)  # 2.015.999.979.840
+
+    # Sum of set with all numbers that contain 0...9 without repetition
+    print(sum_of_set(range(10)))  # 18.143.999.998.185.600
+    print(9_999_999_999 * factorial(10) / 2)  # 18.143.999.998.185.600
 
 
 if __name__ == "__main__":
